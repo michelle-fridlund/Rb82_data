0d2e656bcef45b4b1142280945f4742cc6f0c25f
Michelle Fridlund <michelle.fridlund@gmail.com>
2021-03-17 13:03:18 +0100

    Align loading print, update .gitignore

--- 3d_unet/display_images.py
+++ 3d_unet/display_images.py
@@ -18,19 +18,23 @@ patient = '0eef59dd-8192-4065-b3dc-05f6e00aa452'
 # patient = '0ef7e890-6586-4876-a630-a3af8e7fd736'
 filename = '3_rest-lm-00-psftof_000_000_ctmv_4i_21s.nii.gz'
 
-output = f'/homes/michellef/recon_im/{dose}_{mode.lower()}_rest_{patient}_norm'
+output = f'/homes/michellef/recon_im/{dose}_{mode.lower()}_rest_{patient}_norm_float32'
 im_path = f'/homes/michellef/my_projects/rb82_data/Dicoms_OCT8/{dose}_{mode}/{patient}/{filename}'
-
 
 def mkdir_(output):
     if not os.path.exists(output):
         os.makedirs(output)
-        
+
 # Normalise pixel values
+
+
 def normalise(pixels):
     d_type = pixels.dtype
     return np.array(pixels/65535, dtype=np.dtype(d_type))
+
 # matplotlib
+
+
 def load_nib(im_path):
     import nibabel as nib
     import matplotlib.pyplot as plt
@@ -41,19 +45,24 @@ def load_nib(im_path):
     (a, b, c) = img2.shape
     
     mkdir_(output)
-    # plot for all slices
-    for i in range(0,c):
-        plt.imshow(img2[:,:,i], cmap = 'plasma')
+
+    # # plot for all slices
+    for i in range(0, c):
+        plt.imshow(img2[:,:,i], cmap='plasma')
         plt.axis('off')
+        plt.title('25%')
         plt.colorbar(label='MBq/L', anchor = True)
         plt.savefig(f'{output}/{patient}_{i}')
         plt.close("all")
 
 # antspyx for entire .nii.gz file
+
+
 def load_ants(im_path):
     import ants
     img = ants.image_read(im_path)
-    ants.plot(img, filename=f'/homes/michellef/recon_im/{dose}_{mode.lower()}_rest_{patient}.png', cmap='plasma')
+    ants.plot(img, filename=f'/homes/michellef/my_projects/rb82_data/Dicoms_OCT8/Rb82_denoise_e100_bz1_lr0.0001_k0_predicted/{patient}/100p_{patient}.png', cmap='plasma')
+    # ants.plot(img, filename=f'/homes/michellef/recon_im/{dose}_{mode.lower()}_rest_{patient}.png', cmap='plasma')
 
 
 if __name__ == "__main__":
