1727257cf00ac66701c5601867dc9c2163bde3cd
Michelle Fridlund <michelle.fridlund@gmail.com>
2021-03-23 18:56:40 +0100

    Extract Study/InstanceUID into patient dict

--- src/parse_LM_tags.py
+++ src/parse_LM_tags.py
@@ -11,10 +11,12 @@ import pickle
 import os
 import linecache
 import argparse
+import pandas as pd
 from pathlib import Path
+from ptd_parsers import PetctFileParser
 
 
-# Find listmode files
+# Find patients with LM files
 def find_LM(dir_path):
     pt = Path(dir_path)
     if not pt.is_dir():
@@ -24,14 +26,15 @@ def find_LM(dir_path):
             return f
     return None
 
+
 # Fetch LM info
 def get_dump(new_path):
-    lm = find_LM(new_path)
-    if lm:
-        os.system(f'strings "{lm}" | tail -200 > "{new_path}"/dump.txt')
+    LM = find_LM(new_path)
+    if LM:
+        os.system(f'strings "{LM}" | tail -200 > "{new_path}"/dump.txt')
 
 
-# Find patients
+# Find patients and get info from LISTMODE files
 def find_patients(dir_path):
     patients = {}
     for (dirpath, dirnames, filenames) in os.walk(dir_path):
@@ -41,22 +44,51 @@ def find_patients(dir_path):
         if (new_path/'dump.txt').exists():
             with open(new_path/'dump.txt') as f:
                 for i, line in enumerate(f.readlines()):
-                    if line.startswith('PETCT'): #StudyInstance is before PETCT tag
+                    # StudyInstance is before PETCT tag
+                    if line.startswith('PETCT'):
+                        # Get line before and remove \n
                         prev_line = linecache.getline(f'{str(new_path)}/dump.txt', i).strip()
-                        patients[dirname] = prev_line 
+                        # Return patient name and StudyInstanceUID
+                        patients[dirname] = prev_line
     return patients
-            
+
+
+# Find SeriesInstance UID and create a final patient dictionary
+def get_uid(dir_path):
+    pickle_info = {}
+    patients = find_patients(dir_path)
+
+    for k, v in patients.items():
+        # Patient directories with appropriate files
+        new_path = os.path.join(dir_path, k)
+        # We can only get SeriesInstanceUID from PETCT_SPL files
+        parser = PetctFileParser(new_path)  # Raphael's parser
+
+        if parser.file_in:
+            parser.read_tail(stopword='CTSeriesDicomUIDforAC')
+            parser.get_primary_info()
+            parser.clean_info()
+
+        if parser.info['CTSeriesDicomUIDforAC'] != '':
+            pickle_info[k] = {'StudyInstance': v, 'SeriesInstance': parser.info['CTSeriesDicomUIDforAC']}
+    return pickle_info
+
+
 # Write dump files
 def write_pickle(dir_path):
-    patients = find_patients(dir_path)
+    patients = get_uid(dir_path)
+
     print(f'{len(patients.keys())} PATIENTS FOUND!')
+
     os.chdir(dir_path)
     with open('paediatrics_studyuid.pickle', 'wb') as p:
         pickle.dump(patients, p)
-        
-    print(pickle.load(open('paediatrics_studyuid.pickle','rb')))
 
+    print(pickle.load(open('paediatrics_studyuid.pickle', 'rb')))
 
+    # Nice print statement
+    df = pd.DataFrame(patients).transpose()
+    print(df)
 
 
 if __name__ == "__main__":
