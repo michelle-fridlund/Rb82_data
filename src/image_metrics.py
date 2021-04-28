b97585b3527f977dd796a22ad1dfab7d12a908fb
Michelle Fridlund <michelle.fridlund@gmail.com>
2021-04-28 17:51:39 +0200

    Implement nifti/volume assessment

--- src/image_metrics.py
+++ src/image_metrics.py
@@ -12,14 +12,28 @@ import glob
 import argparse
 import pickle
 import numpy as np
+import nibabel as nib
 from math import log10, sqrt
 import matplotlib.pyplot as plt
 from pathlib import Path
+
+folder = '3_rest-lm-00-psftof_000_000_ctmv_4i_21s'
 
 
 def load_images(path):
     return [plt.imread(i) for i in glob.glob("{}/*.png".format(path), recursive=True)]
 
+def find_nifti(path):
+    return [nib.load(i) for i in glob.glob("{}/*.nii.gz".format(path), recursive=True)]
+
+def normalise(img, **dose_):
+    d_type = img.header.get_data_dtype()
+    np_im = np.array(img.get_fdata(), dtype=np.dtype(d_type))
+    if dose_.get("dose_") == "ld":
+        return np.array(np_im*4/(65535), dtype=np.dtype(d_type))
+    else:
+        return np.array(np_im/(65535), dtype=np.dtype(d_type))
+    
 
 # Return PSNR as compared to respective target
 def psnr_(hd, ld):
@@ -54,19 +68,42 @@ def err(value):
     return np.std(value)/np.sqrt(len(value))
 
 
+# Create a dictionary where patients are keys with hd/ld values
+def find_patients(args):
+    patient_dict = {}
+    patients = read_pickle(str(args.pkl_path))
+    
+    # Change this to {p}_rest/stress_predicted for inferecd
+    for p in patients:
+        if str(args.mode) == 'numpy':
+            patient_dict[p] = {'hd': os.path.join(
+                str(args.hd), p, folder), 'ld': os.path.join(str(args.ld), p, f'{p}_rest_predicted')}
+    
+        elif str(args.mode) == 'nifti':
+            patient_dict[p] = {'hd': os.path.join(
+                str(args.hd), p), 'ld': os.path.join(str(args.ld), p)}
+            
+    return patient_dict
+
+
 # Create a dictionary with all metrics for low-dose directory
-def get_metrics(hd_path, ld_path):
+def get_metrics(args, hd_path, ld_path):
 
     metrics = {'psnr': [],
                'ssim': [],
                'nrmse': [],
                'cv_hd': [],
                'cv_ld': [], }
-
-    hd = load_images(hd_path)
-    ld = load_images(ld_path)
-    # print(f'{len(hd)} hd and {len(ld)} ld images found')
-
+    
+    if str(args.mode) == 'numpy':
+        hd = load_images(hd_path)
+        ld = load_images(ld_path)
+    
+    if str(args.mode) == 'nifti':
+        hd = normalise(nib.load(os.path.join(str(hd_path), f'{folder}.nii.gz')))
+        ld = normalise(nib.load(os.path.join(str(ld_path), f'{folder}.nii.gz')), dose_='ld')
+        
+        
     for im_hd, im_ld in zip(hd, ld):
         metrics['psnr'].append(psnr_(im_hd, im_ld))
         metrics['ssim'].append(ssim_(im_hd, im_ld))
@@ -85,16 +122,14 @@ def read_pickle(pkl_file):
 
 
 # Write metrics into pkl
-def build_pickle(hd_path, ld_path):
-    metrics = get_metrics(hd_path, ld_path)
+def build_pickle(args, hd_path, ld_path):
+    metrics = get_metrics(args, hd_path, ld_path)
     # here = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(ld_path, 'im_metrics.pickle'), 'wb') as p:
+    with open(os.path.join(ld_path, 'im_metrics_nifti.pickle'), 'wb') as p:
         pickle.dump(metrics, p)
     print('.')
 
 # Extract info from pkl
-
-
 def get_stats(p):
     psnr = p['psnr']
     ssim = p['ssim']
@@ -103,22 +138,8 @@ def get_stats(p):
     cv_ld = p['cv_ld']
     return psnr, ssim, nrmse, cv_hd, cv_ld
 
-# Create a dictionary where patients are keys with hd/ld values
-
-
-def find_patients(args):
-    patient_dict = {}
-    patients = read_pickle(str(args.pkl_path))
-    # Change this to {p}_rest/stress_predicted for inferecd
-    folder = '3_stress-lm-00-psftof_000_000_ctmv_4i_21s'
-    for p in patients:
-        patient_dict[p] = {'hd': os.path.join(
-            str(args.hd), p, folder), 'ld': os.path.join(str(args.ld), p, folder)}
-    return patient_dict
 
 # Append individual metric values for all image slices for all patient keys
-
-
 def evaluate_patients(args):
     patient_dict = find_patients(args)
     stats_dict = {}
@@ -130,9 +151,10 @@ def evaluate_patients(args):
                              'cv_hd': [],
                              'cv_ld': [], }
             # Check if pkl exists already
-            if not (Path('%s/im_metrics.pickle' % v['ld'])).exists():
-                build_pickle(v['hd'], v['ld'])
-            p = pickle.load(open('%s/im_metrics.pickle' % v['ld'], 'rb'))
+            # os.remove('%s/im_metrics_nifti.pickle' % v['ld'])
+            if not (Path('%s/im_metrics_nifti.pickle' % v['ld'])).exists():
+                build_pickle(args, v['hd'], v['ld'])
+            p = pickle.load(open('%s/im_metrics_nifti.pickle' % v['ld'], 'rb'))
             # TODO: technically could populate an array here, don't need dict
             # and reduce by 1 fucntion
             psnr, ssim, nrmse, cv_hd, cv_ld = get_stats(p)
@@ -173,8 +195,9 @@ def overall_stats(args):
     print(f"PSNR value is: {np.mean(psnr):.4f} + {err(psnr):.4f}")
     print(f"SSIM value is: {np.mean(ssim):.4f} + {err(ssim):.4f}")
     print(f"NRMSE value is: {np.mean(nrmse):.4f} + {err(nrmse):.4f}")
-    print(
-        f"CV in target: {np.mean(cv_hd):.4f}% + {err(cv_hd):.4f}%; CV in low-dose: {np.mean(cv_ld):.4f}% + {err(cv_ld):.4f}")
+    print(f"CV in low-dose: {np.mean(cv_ld):.4f}% + {err(cv_ld):.4f}")
+    # print(
+    #     f"CV in target: {np.mean(cv_hd):.4f}% + {err(cv_hd):.4f}%; CV in low-dose: {np.mean(cv_ld):.4f}% + {err(cv_ld):.4f}")
 
 
 if __name__ == "__main__":
@@ -187,15 +210,21 @@ if __name__ == "__main__":
         "--hd", dest='hd',  help="Path to target image directory", required=True)
     required_args.add_argument(
         "--ld", dest='ld',  help="Path to low-dose image directory", required=True)
+    
+    # Process in numpy or nifti format
+    required_args.add_argument(
+        "--mode", dest='mode',  help="numpy/nifti", required=True)
 
     # Specify a pkl file for list of patients
     parser.add_argument('--pkl', dest='pkl_path', help="pickle file path")
+    
 
     # Read arguments from the command line
     args = parser.parse_args()
 
     hd_path = args.hd
     ld_path = args.ld
+    
 
     if args.pkl_path:
         overall_stats(args)
