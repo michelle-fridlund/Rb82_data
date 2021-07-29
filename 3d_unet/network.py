9262f48912dbca0c3ad078c53767126a48012a29
Michelle Fridlund <michelle.fridlund@gmail.com>
2021-07-29 16:29:42 +0200

    Formatting code, removing extra comments

--- 3d_unet/network.py
+++ 3d_unet/network.py
@@ -39,65 +39,35 @@ def convt_block(layer, concat, fsize):
     layer = concatenate([layer, concat], axis=-1)
     return layer
 
+
 # Filters like Chen et al.
-
-
 def construct_3D_unet_architecture(X):
 
     # ENCODING
-    # block1, dblock1 = conv_block(X, 64, .1)  # 64x64x16
-    # block2, dblock2 = conv_block(dblock1, 128, .1)  # 32x32x8
-    # block3, dblock3 = conv_block(dblock2, 256, .2)  # 16x16x4
-    # block4, _ = conv_block(dblock3, 512, .3, downsample=False)
+    block1, dblock1 = conv_block(X, 64, .1)  # 64x64x16
+    block2, dblock2 = conv_block(dblock1, 128, .1)  # 32x32x8
+    block3, dblock3 = conv_block(dblock2, 256, .2)  # 16x16x4
+    block4, _ = conv_block(dblock3, 512, .3, downsample=False)
 
-    # # DECODING
-    # block5 = convt_block(block4, block3, 256)
-    # block6, _ = conv_block(block5, 256, .2, downsample=False)
+    # DECODING
+    block5 = convt_block(block4, block3, 256)
+    block6, _ = conv_block(block5, 256, .2, downsample=False)
 
-    # block7 = convt_block(block6, block2, 128)
-    # block8, _ = conv_block(block7, 128, .2, downsample=False)
+    block7 = convt_block(block6, block2, 128)
+    block8, _ = conv_block(block7, 128, .2, downsample=False)
 
-    # block9 = convt_block(block8, block1, 64)
-    # block10, _ = conv_block(block9, 64, .1, downsample=False)
-    
-    # ENCODING
-    block1, dblock1 = conv_block(X, 32, .1)  #32 kernels  used
-    block2, dblock2 = conv_block(dblock1, 64, .1)  
-    block3, dblock3 = conv_block(dblock2, 128, .2)  
-    block4, dblock4 = conv_block(dblock3, 256, .2)  
-    block5, _ = conv_block(dblock4, 512, .3, downsample=False)
+    block9 = convt_block(block8, block1, 64)
+    block10, _ = conv_block(block9, 64, .1, downsample=False)
 
-    block6 = convt_block(block5, block4, 256)
-    block7, _ = conv_block(block6, 256, .3, downsample=False)
-
-    block8 = convt_block(block7, block3, 128)
-    block9, _ = conv_block(block8, 128, .2, downsample=False)
-
-    block10 = convt_block(block9, block2, 64)
-    block11, _ = conv_block(block10, 64, .2, downsample=False)
-    
-    block12 = convt_block(block11, block1, 32)
-    block13, _ = conv_block(block12, 32, .1, downsample=False)
-   
     output = Conv3D(1, (3, 3, 3), kernel_regularizer=regularizers.l2(1e-1),
-                    kernel_initializer='he_normal', padding='same', 
-                    strides=(1, 1, 1), activation='relu')(block13)
+                    kernel_initializer='he_normal', padding='same',
+                    strides=(1, 1, 1), activation='relu')(block10)
 
     return output
 
-# def get_lr_metric(model):
-#     def lr(y_true, y_pred):
-#         optimizer = model.optimizer
-#         iterations = optimizer.iterations
-#         decay = optimizer.lr
-#         lr = decay.decayed_learning_rate(iterations)
-#         # decay = optimizer.lr
-#         # return K.eval(lr.decay)
-#         # # return K.eval(K.cast(decay, K.dtype(lr)))
-#     return lr
-
 
 def prepare_3D_unet(x, y, z, d, initialize_model=None, classification=False, lr=0.0001, loss='mean_absolute_error'):
+
     inp = Input(shape=(x, y, z, d))
     output = construct_3D_unet_architecture(inp)
     model = Model(inputs=inp, outputs=output)
@@ -110,20 +80,11 @@ def prepare_3D_unet(x, y, z, d, initialize_model=None, classification=False, lr=
         if os.path.exists(initialize_model):
             # load weights
             model.load_weights(initialize_model)
-            print("Created model and loaded weights from file: %s" % initialize_model)
+            print("Created model and loaded weights from file: %s" %
+                  initialize_model)
         else:
             exit('did not find model %s' % initialize_model)
-            
-    # initial_learning_rate = lr
-    # # initial_learning_rate = tf.cast(initial_learning_rate, tf.float64)
-    # lr_schedule = ExponentialDecay(
-    # initial_learning_rate,
-    # decay_steps=1000,
-    # decay_rate=0.95)
-    
-    # lr_metric = get_lr_metric(model)
-    # lr_metric = tf.cast(lr_metric, tf.float64)
-    # model.compile(optimizer=Adam(learning_rate=lr_schedule), loss='mean_absolute_error', metrics=['mean_absolute_error', 'acc'])
-    model.compile(optimizer=Adam(lr=lr), loss='mean_absolute_error', metrics=['mean_absolute_error', 'acc'])
-    # print('Learning rate: ' + str(round(model.optimizer.lr.numpy(), 5)))
+
+    model.compile(optimizer=Adam(lr=lr), loss='mean_absolute_error',
+                  metrics=['mean_absolute_error', 'acc'])
     return model
--- 3d_unet/network_v2.py
+++ 3d_unet/network_v2.py
@@ -37,37 +37,34 @@ def convt_block(layer, concat, fsize):
     layer = concatenate([layer, concat], axis=-1)
     return layer
 
-# Filters like Chen et al.
 
-
+# Filters 64 to 1024
 def construct_3D_unet_architecture(X):
 
-
     # ENCODING
-    block1, dblock1 = conv_block(X, 32, .1) #32 kernels  # 32x32x16
-    block2, dblock2 = conv_block(dblock1, 64, .1) # 64x64x8
-    block3, dblock3 = conv_block(dblock2, 128, .2) # 128x128x4
-    block4, dblock4 = conv_block(dblock3, 256, .2)  
-    
-    block5, _ = conv_block(dblock4, 512, .3, downsample=False)
+    block1, dblock1 = conv_block(X, 64, .1)  # 32 kernels  # 32x32x16
+    block2, dblock2 = conv_block(dblock1, 128, .1)  # 64x64x8
+    block3, dblock3 = conv_block(dblock2, 256, .2)  # 128x128x4
+    block4, dblock4 = conv_block(dblock3, 512, .2)
+
+    block5, _ = conv_block(dblock4, 1024, .3, downsample=False)
 
     # DECODING
-    block6 = convt_block(block5, block4, 256)
-    block7, _ = conv_block(block6, 256, .3, downsample=False)
+    block6 = convt_block(block5, block4, 512)
+    block7, _ = conv_block(block6, 512, .3, downsample=False)
 
-    block8 = convt_block(block7, block3, 128)
-    block9, _ = conv_block(block8, 128, .2, downsample=False)
+    block8 = convt_block(block7, block3, 256)
+    block9, _ = conv_block(block8, 256, .2, downsample=False)
 
-    block10 = convt_block(block9, block2, 64)
-    block11, _ = conv_block(block10, 64, .2, downsample=False)
-    
-    block12 = convt_block(block11, block1, 32)
-    block13, _ = conv_block(block12, 32, .1, downsample=False)
-   
+    block10 = convt_block(block9, block2, 128)
+    block11, _ = conv_block(block10, 128, .2, downsample=False)
+
+    block12 = convt_block(block11, block1, 64)
+    block13, _ = conv_block(block12, 64, .1, downsample=False)
+
     output = Conv3D(1, (3, 3, 3), kernel_regularizer=regularizers.l2(1e-1),
-                    kernel_initializer='he_normal', padding='same', 
+                    kernel_initializer='he_normal', padding='same',
                     strides=(1, 1, 1), activation='relu')(block13)
-
 
     return output
 
@@ -86,10 +83,11 @@ def prepare_3D_unet(x, y, z, d, initialize_model=None, classification=False, lr=
         if os.path.exists(initialize_model):
             # load weights
             model.load_weights(initialize_model)
-            print("Created model and loaded weights from file: %s" % initialize_model)
+            print("Created model and loaded weights from file: %s" %
+                  initialize_model)
         else:
             exit('did not find model %s' % initialize_model)
-            
-    
-    model.compile(optimizer=Adam(lr=lr), loss='mean_absolute_error', metrics=['mean_absolute_error', 'acc'])
+
+    model.compile(optimizer=Adam(lr=lr), loss='mean_absolute_error',
+                  metrics=['mean_absolute_error', 'acc'])
     return model
