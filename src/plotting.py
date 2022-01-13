df008e1b65051fb0b16885b3d4ab9f6df03beb89
Michelle Fridlund <michelle.fridlund@gmail.com>
2022-01-13 18:06:34 +0100

    Extract net true counts for selected dose ranges

--- src/plotting.py
+++ src/plotting.py
@@ -4,6 +4,7 @@
 Created on Tue Jan 11 2022 
 @author: michellef
 """
+import os
 import xlrd
 import seaborn as sns
 import pandas as pd
@@ -14,7 +15,10 @@ from datetime import datetime
 
 
 loc = ("/homes/michellef/rb82_doses.xlsx")
+data_path = "/homes/michellef/my_projects/rb82_data/Dicoms_OCT8/100p_STAT"
+file_name = "REST/Sinograms/REST-LM-00-sino-0.s.hdr"
 
+# Extract indivudual column values from excel file
 def read_x(loc):
     wb = xlrd.open_workbook(loc)
     sheet = wb.sheet_by_index(0)
@@ -22,25 +26,28 @@ def read_x(loc):
     
     doses = []
     dates = []
+    patients = []
     for i in range(sheet.nrows):
         doses.append(sheet.cell_value(i, 2))
         dates.append(sheet.cell_value(i, 1))
+        patients.append(sheet.cell_value(i, 0))
+    return doses, dates, patients
 
-    return doses, dates
 
 def plot(loc):
-    doses, dates = read_x(loc)
+    doses, dates, patients = read_x(loc)
     dpd = pd.Series(doses)
 
     dpd.plot.hist(grid=False, bins=20, rwidth=0.9, color='#607c8e')
     plt.title('Rb-82 doses for 168 patients')
     plt.xlabel('Injected dose [MBq]')
     plt.ylabel('Patients')
-    plt.savefig('/homes/michellef/clinical_eval/test.png')
+    plt.savefig('/homes/michellef/clinical_eval/hist.png')
     plt.close("all")
 
+
 def plot_dates(loc):
-    doses, dates = read_x(loc)
+    doses, dates, patients = read_x(loc)
 
     df = pd.DataFrame(columns=['dates','doses'])
     for k,v in zip(doses, dates):
@@ -49,8 +56,7 @@ def plot_dates(loc):
     df['dates'] = pd.to_datetime(df['dates'], format='%Y%m%d')
     df.doses = df.doses.astype('float')
     ordered_df = df.sort_values(by='dates')
-    
-    #df.plot.scatter(x='dates', y='doses')
+
     pd.plotting.register_matplotlib_converters()
     plt.figure(figsize = (8,5))
     ax = sns.scatterplot(data = ordered_df, x = 'dates', y = 'doses', marker="x", color='crimson')
@@ -59,4 +65,43 @@ def plot_dates(loc):
     plt.savefig('/homes/michellef/clinical_eval/scatter.png')
     plt.close("all")
 
-plot_dates(loc)
+
+def find_patients(loc):
+    retained = []
+
+    doses, dates, patients = read_x(loc)
+    for k,v in zip(doses, patients):
+        k = float(k)
+        if k >= 1000.0 and k <= 1200.0:
+            retained.append(v)
+    
+    print(f'{len(retained)} suitable patients found')
+    return retained 
+
+def get_counts(loc):
+    patient_list = find_patients(loc)
+    counts = []
+
+    for p in patient_list:
+        sino_path = os.path.join(data_path, p, file_name)
+        with open(sino_path) as f:
+            for line in f.readlines():
+                line_ = line.strip()
+                if line_.startswith('%total net trues:='):
+                    c = line_.split(':=')[1]
+                    counts.append(float(c))
+    return counts
+
+def plot_counts(loc):
+    counts = get_counts(loc)
+    
+    dpd = pd.Series(counts)
+
+    dpd.plot.hist(grid=False, bins=15, rwidth=0.9, color='#F55D3C')
+
+    plt.title('Net trues for doses in the 1000-1200 MBq range')
+    plt.xlabel('Total net trues')
+    plt.savefig('/homes/michellef/clinical_eval/test.png')
+    plt.close("all")
+
+plot_counts(loc)
