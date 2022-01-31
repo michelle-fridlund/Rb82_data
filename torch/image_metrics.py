310990070cd7b4cb3af0304f8fd86bbdb8f570db
Michelle Fridlund <michelle.fridlund@gmail.com>
2022-01-31 17:58:29 +0100

    Inference folder 2022 created

--- torch/image_metrics.py
+++ torch/image_metrics.py
@@ -9,6 +9,7 @@ import os
 import argparse
 import pickle
 import numpy as np
+import scipy
 import nibabel as nib
 from math import log10, sqrt
 import matplotlib.pyplot as plt
@@ -75,17 +76,16 @@ def find_patients(args):
     ld = f'pet_25p_stat_norm.nii.gz'
     #out1 = f'test_LightningAE_UNET3D_newsplit_v1_TIODataModule_bz4_128x128x16_k0_e600_e=490.nii.gz'
     #out2 = f'test_LightningAE_ResUNET3D_newsplit_TIODataModule_bz4_128x128x16_k0_e600_e=506.nii.gz'
-    #out4 = f'test_LightningRAE_Res3DUnet_residual_TIODataModule_bz4_128x128x16_k0_e600_e=506.nii.gz'
-    out1 = f'test_LightningAE_FAN2D_v2_TIODataModule_bz4_128x128x1_k0_e400_e=112.nii.gz'
-    out2 = f'test_LightningAE_FAN2D_v3_TIODataModule_bz16_128x128x1_k0_e400_e=192.nii.gz'
-    out3 = f'FAN_LightningAE_FAN2D_v4_TIODataModule_bz16_128x128x1_k0_e400_e=228.nii.gz'
-
+    #out1 = f'test_LightningRAE_Res3DUnet_residual_TIODataModule_bz4_128x128x16_k0_e600_e=506.nii.gz'
+    #out1 = f'test_LightningAE_Res3DUnet_2mm_2mm_TIODataModule_bz4_128x128x16_k0_e600_e=214.nii.gz'
+    #out2 = f'test_LightningRAE_Res3DUnet_RAE_2mm_TIODataModule_bz4_128x128x16_k0_e600_e=502.nii.gz'
+    out1 = f'LightningRAE_Res3DUnet_dosescaled_TIODataModule_bz4_128x128x16_k0_e600_e=442.nii.gz'
     for p in patients:
         patient_dict[p] = {'hd': os.path.join(
             str(args.data), p, hd), 'ld': os.path.join(str(args.data), p, ld),
             'out1': os.path.join(str(args.inference), p, out1),
-            'out2': os.path.join(str(args.inference), p, out2),
-            'out3': os.path.join(str(args.inference), p, out3),
+            #'out2': os.path.join(str(args.inference), p, out2),
+            #'out3': os.path.join(str(args.inference), p, out3),
             #'out4': os.path.join(str(args.inference), p, out4)
             }
 
@@ -122,14 +122,14 @@ def get_stats(args):
 
     metrics = get_metrics(args, ld_type = 'ld')
     metrics_inference1 = get_metrics(args, ld_type = 'out1')
-    metrics_inference2 = get_metrics(args, ld_type = 'out2')
-    metrics_inference3 = get_metrics(args, ld_type = 'out3')
+    #metrics_inference2 = get_metrics(args, ld_type = 'out2')
+    #metrics_inference3 = get_metrics(args, ld_type = 'out3')
     #metrics_inference4 = get_metrics(args, ld_type = 'out4')
 
     psnr, ssim, nrmse = return_values(metrics)
     psnr2, ssim2, nrmse2 = return_values(metrics_inference1)
-    psnr3, ssim3, nrmse3 = return_values(metrics_inference2)
-    psnr4, ssim4, nrmse4 = return_values(metrics_inference3)
+    #psnr3, ssim3, nrmse3 = return_values(metrics_inference2)
+    #psnr4, ssim4, nrmse4 = return_values(metrics_inference3)
     #psnr5, ssim5, nrmse5 = return_values(metrics_inference4)
 
     print('\n\n')
@@ -138,25 +138,27 @@ def get_stats(args):
     print(f"SSIM value is: {np.mean(ssim):.4f} + {err(ssim):.4f}")
     print(f"NRMSE value is: {np.mean(nrmse):.4f} + {err(nrmse):.4f}")
 
+
     print('\n\n 2')
-    print('Inference: ')
     print(f"PSNR value is: {np.mean(psnr2):.4f} + {err(psnr2):.4f}")
     print(f"SSIM value is: {np.mean(ssim2):.4f} + {err(ssim2):.4f}")
     print(f"NRMSE value is: {np.mean(nrmse2):.4f} + {err(nrmse2):.4f}")
 
+    return metrics, metrics_inference1
+"""    
     print('\n\n 3')
     print('3')
     print(f"PSNR value is: {np.mean(psnr3):.4f} + {err(psnr3):.4f}")
     print(f"SSIM value is: {np.mean(ssim3):.4f} + {err(ssim3):.4f}")
     print(f"NRMSE value is: {np.mean(nrmse3):.4f} + {err(nrmse3):.4f}")
-    
+
     print('\n\n 4')
     print('Inference: ')
-    print(f"PSNR value is: {np.mean(psnr4):.4f} + {err(psnr4):.4f}")
+    print(f"PSNR value is: {np.mean(psnr4):.4f} s+ {err(psnr4):.4f}")
     print(f"SSIM value is: {np.mean(ssim4):.4f} + {err(ssim4):.4f}")
     print(f"NRMSE value is: {np.mean(nrmse4):.4f} + {err(nrmse4):.4f}")
 
-    return metrics
+    return metrics """
 """     print('\n\n residual')
     print('Inference: ')
     print(f"PSNR value is: {np.mean(psnr5):.4f} + {err(psnr5):.4f}")
@@ -166,13 +168,24 @@ def get_stats(args):
 
 
 def print_values(args):
-    metrics, metrics_inference3 = get_stats(args)
+    metrics, metrics_inference1 = get_stats(args)
     data = pd.DataFrame.from_dict(metrics)
-    data2 = pd.DataFrame.from_dict(metrics_inference3)
+    #data2 = pd.DataFrame.from_dict(metrics_inference1)
+""" 
+    df = pd.DataFrame(columns=['before','after',])
+    for k,v in zip(metrics['nrmse'], metrics_inference1['nrmse']):
+        df = df.append({'before': k, 'after':v},ignore_index=True)
+    df.before = df.before.astype('float')
+    df.after = df.after.astype('float')
 
-    concatenated = pd.concat([data.assign(image='Original'), data2.assign(image='AI-Improved')])
-    sns.boxplot(x=concatenated.image, y = concatenated.psnr, data = concatenated)
-    plt.savefig('/homes/michellef/16Nov_stats.png')
+    # Paired t-test
+    from bioinfokit.analys import stat
+    res = stat()
+    res.ttest(df = df, res = ['after', 'before'], test_type = 3)
+    print(res.summary) """
+    #concatenated = pd.concat([data.assign(image='Low Dose'), data2.assign(image='AI-Improved')])
+    #sns.boxplot(x=concatenated.image, y = concatenated.nrmse, data = concatenated)
+    #plt.savefig('/homes/michellef/clinical_eval/21Jan_nrmse.png')
 
 if __name__ == "__main__":
     # Initiate the parser
@@ -196,6 +209,6 @@ if __name__ == "__main__":
     # Read arguments from the command line
     args = parser.parse_args()
 
-    get_stats(args)
-
+    #get_stats(args)
+    print_values(args)
     print('Done.')
--- torch/statistics.py
+++ torch/statistics.py
@@ -20,14 +20,14 @@ patients = ['6db1c5da-8e89-4287-8a8b-57c64ed0109b', '6fa399c0-ad77-4e0c-bea8-37c
 
 """ # Static
 extent_stress_100 = [10, 30, 29, 0, 3, 0, 13, 27, 1, 16, 1]
-extent_rest_100 = [1, 15, 0, 0, 2, 0, 2, 11, 0, 5, 0]
+    extent_rest_100 = [1, 15, 0, 0, 2, 0, 2, 11, 0, 5, 0]
 tpd_stress_100 = [8,22,21,1,2,0,10,20,1,12,1]
 tpd_rest_100 = [2,10,0,0,1,0,2,8,0,6,0]
+tpd_stress_25 = [6,23,24,2,4,1,10,22,2,13,4]
+tpd_rest_25 = [1,11,0,0,2,0,3,8,0,5,1]
 
 extent_stress_25 = [8,32,33,2,5,1,13,30,1,16,5]
 extent_rest_25 = [2,15,0,0,4,0,4,10,0,5,1]
-tpd_stress_25 = [6,23,24,2,4,1,10,22,2,13,4]
-tpd_rest_25 = [1,11,0,0,2,0,3,8,0,5,1]
 
 extent_stress_12 = [10, 34, 34, 8, 7, 2, 17, 33, 3, 24, 12]
 extent_rest_12 = [3, 17, 2, 2, 3, 0, 5, 13, 1, 10, 2]
@@ -52,9 +52,9 @@ tpd_rest_out = [1,9,0,0,2,0,1,7,0,4,0]
 
 # 2mm/2mm
 extent_stress_2mm = [5,37,31,1,2,0,14,29,2,15]
-extent_rest_2mm = [1,15,0,0,2,0,3,10,0,4]
 tpd_stress_2mm = [5,26,22,1,2,1,11,22,1,12]
 tpd_rest_2mm = [1,11,0,0,1,0,3,8,0,5]
+extent_rest_2mm = [1,15,0,0,2,0,3,10,0,4]
 
 # Gated
 edv_stress_100 = [145,53,94,104,82,76,98,165,83,111,195]
@@ -137,9 +137,23 @@ ef_rest_100 = [48,65,61,72,54,79,44,40,71]
 ef_stress_25 = [50,68,42,62,61,82,68,28,65]
 ef_rest_25 = [48,68,52,69,52,77,56,35,67]
 
-df2 = pd.DataFrame({'ef_resnet_gate_stress': [47,75,43,66,67,83,68,37,71],
-                    'ef_stress_100': [48,73,42,53,55,82,63,29,70],
-                    'ef_stress_25': [50,68,42,62,61,82,68,28,65],},
+tpd_stress_100 = [22,21,1,2,0,10,20,1,12,1]
+tpd_stress_25 = [23,24,2,4,1,10,22,2,13,4]
+tpd_stress_out = [21,22,0,2,0,9,20,0,9,1]
+tpd_rest_100 = [10,0,0,1,0,2,8,0,6,0]
+tpd_rest_25 = [11,0,0,2,0,3,8,0,5,1]
+tpd_rest_out = [9,0,0,2,0,1,7,0,4,0]
+
+
+df2 = pd.DataFrame({#'ef_resnet_gate_stress': [47,75,43,66,67,83,68,37,71],
+                    #'ef_stress_100': [48,73,42,53,55,82,63,29,70],
+                    #'ef_stress_25': [50,68,42,62,61,82,68,28,65],
+                    'tpd_rest_100': [10,0,0,1,0,2,8,0,6,0],
+                    'tpd_rest_25': [11,0,0,2,0,3,8,0,5,1],
+                    'tpd_rest_out': [9,0,0,2,0,1,7,0,4,0],
+                    'tpd_stress_100': [22,21,1,2,0,10,20,1,12,1],
+                    'tpd_stress_25': [23,24,2,4,1,10,22,2,13,4],
+                    'tpd_stress_out': [21,22,0,2,0,9,20,0,9,1],},
                     index=['6db1c5da-8e89-4287-8a8b-57c64ed0109b', 
                            '6fa399c0-ad77-4e0c-bea8-37ce8ab36dbf', 
                            '28c4f1c2-2cbb-489e-ab57-09b13a60231c', 
@@ -148,44 +162,47 @@ df2 = pd.DataFrame({'ef_resnet_gate_stress': [47,75,43,66,67,83,68,37,71],
                            '4019e4ab-b8bd-40b3-bb1d-f176feb13384',
                            'b48007ee-dbe4-4fe9-a7e2-7c3020f468b7',
                            '0ef7e890-6586-4876-a630-a3af8e7fd736',
-                           '7b4979e9-767d-422c-9cec-f80f787ded49'])
+                           '7b4979e9-767d-422c-9cec-f80f787ded49',
+                           '500cfb2b-e287-4e4c-8143-273524f7564b',])
 
 df = pd.DataFrame(columns=['clinical','low','dose'])
-for k,v in zip(ef_stress_100,ef_stress_25):
+for k,v in zip(tpd_rest_100,tpd_rest_25):
     df = df.append({'clinical': k, 'low':v, 'dose':'25%'},ignore_index=True)
-for k,v in zip(ef_stress_100,ef_resnet_gate_stress): # HERE!!!
+for k,v in zip(tpd_rest_100,tpd_rest_out): # HERE!!!
     df = df.append({'clinical': k, 'low':v, 'dose':'25% (Denoised)'},ignore_index=True) # HERE!!!
 df.clinical = df.clinical.astype('float')
 df.low = df.low.astype('float')
 
 #line_kws={'label':"y={0:.2f}x+{1:.2f}".format(slope,intercept)}    # HERE!!!
-slope, intercept, r_value, p_value, std_err = stats.linregress(df2['ef_stress_100'],df2['ef_stress_25'])  
-slope2, intercept2, r_value2, p_value2, std_err2 = stats.linregress(df2['ef_stress_100'],df2['ef_resnet_gate_stress']) # HERE!!!
+slope, intercept, r_value, p_value, std_err = stats.linregress(df2['tpd_rest_100'],df2['tpd_rest_25'])  
+slope2, intercept2, r_value2, p_value2, std_err2 = stats.linregress(df2['tpd_rest_100'],df2['tpd_rest_out']) # HERE!!!
 print(p_value, p_value2)
 
 fgrid = sns.lmplot(x="clinical", y="low", data=df, hue = 'dose', palette="Set1")
 ax = fgrid.axes[0,0]   # HERE!!! p={0:.4f}".format(p_value)
 #ax.set(xlabel="Clinical MBF rest [mL/(min·g)]", ylabel="Low-Dose MBF rest [mL/(min·g)]") 
-ax.set(xlabel="Clinical LVEF stress [%]", ylabel="Low-Dose LVEF stress [%]") 
-plt.text(20, 80, "y={0:.2f}x+{1:.2f}".format(slope,intercept), horizontalalignment='left', size='medium', color='crimson', weight='semibold')
-plt.text(20, 75, "R={0:.2f}".format(r_value), horizontalalignment='left', size='medium', color='crimson', weight='semibold')
-plt.text(20, 70, "p=5.0484e-05", horizontalalignment='left', size='medium', color='crimson', weight='semibold')
-plt.text(50, 40, "y={0:.2f}x+{1:.2f}".format(slope2, intercept2), horizontalalignment='left', size='medium', color='steelblue', weight='semibold')
-plt.text(50, 35, "R={0:.2f}".format(r_value2), horizontalalignment='left', size='medium', color='steelblue', weight='semibold')
-plt.text(50, 30, "p=8.7527e-05", horizontalalignment='left', size='medium', color='steelblue', weight='semibold')
-#list1 = [i for i in range (1,25)]
+ax.set(xlabel="Clinical TPD rest [%]", ylabel="Low-Dose TPD rest [%]") 
+plt.text(2, 9, "y={0:.2f}x+{1:.2f}".format(slope,intercept), horizontalalignment='left', size='medium', color='crimson', weight='semibold')
+plt.text(2, 8.2, "R={0:.2f}".format(r_value), horizontalalignment='left', size='medium', color='crimson', weight='semibold')
+plt.text(2, 7.4, "p=2.4047e-07", horizontalalignment='left', size='medium', color='crimson', weight='semibold')
+plt.text(6, 2, "y={0:.2f}x+{1:.2f}".format(slope2, intercept2), horizontalalignment='left', size='medium', color='steelblue', weight='semibold')
+plt.text(6, 1.2, "R={0:.2f}".format(r_value2), horizontalalignment='left', size='medium', color='steelblue', weight='semibold')
+plt.text(6, 0.4, "p=3.5978e-07", horizontalalignment='left', size='medium', color='steelblue', weight='semibold')
+#list1 = [i for i in range (1,80)]
 #plt.plot(list1, linewidth=2, linestyle='--', color = 'black')
-plt.savefig('/homes/michellef/clinical_eval/DEC15.png')
+#plt.xlim(20,90)
+#plt.ylim(20,100)
+#plt.savefig('/homes/michellef/clinical_eval/abstract/tpd_rest.png')
 plt.close()
 
-""" # The horizontal plot is made using the hline function
+# The horizontal plot is made using the hline function
 my_range=range(1,len(df2.index)+1)
 
 ordered_df = df2.sort_values(by='tpd_stress_100')
 
-plt.hlines(y=my_range, xmin=ordered_df['tpd_stress_100'], xmax=ordered_df['tpd_stress_25'], color='grey', alpha=0.4)
+plt.hlines(y=my_range, xmin=ordered_df['tpd_stress_100'], xmax=ordered_df['tpd_stress_out'], color='grey', alpha=0.4)
 plt.scatter(ordered_df['tpd_stress_100'], my_range, color='crimson', alpha=0.4 , label='100%')
-plt.scatter(ordered_df['tpd_stress_25'], my_range, color='skyblue', alpha=1, label='25%')
+plt.scatter(ordered_df['tpd_stress_out'], my_range, color='skyblue', alpha=1, label='25%')
 
 plt.legend(loc='lower right')
 plt.xlabel('TPD stress [%]')
@@ -204,8 +221,10 @@ plt.text(10.6, 11.5, "Moderate", horizontalalignment='left', size='medium', colo
 plt.text(10.0, 11.0, "abnormality", horizontalalignment='left', size='medium', color='r', alpha = 0.5) 
 plt.axvline(x=15.0, color='brown', linestyle='--', alpha = 0.4)
 plt.text(19.4, 11.5, "Severe", horizontalalignment='left', size='medium', color='brown', alpha = 0.5)
-plt.text(18.6, 11.0, "abnormality", horizontalalignment='left', size='medium', color='brown', alpha = 0.5)  """
+plt.text(18.6, 11.0, "abnormality", horizontalalignment='left', size='medium', color='brown', alpha = 0.5) 
 
+plt.savefig('/homes/michellef/clinical_eval/abstract/out_stress.png')
+plt.close()
 
 """ plt.axvline(x=1.5, color='r', linestyle='--', alpha = 0.4)
 plt.text(0.9, 11.0, "Elevated cardiac risk", horizontalalignment='left', size='medium', color='r', alpha = 0.5)
@@ -228,6 +247,3 @@ plt.text(55.0, 11.2, "Normal range", horizontalalignment='left', size='medium', 
 plt.text(72.0, 11.2, "Hyperdynamic", horizontalalignment='left', size='medium', color='steelblue', alpha = 0.5)
 
 plt.xlim(25,85) """
-
-#plt.savefig('/homes/michellef/clinical_eval/TEST2.png')
-#plt.close()
