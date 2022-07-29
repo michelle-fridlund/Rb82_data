117bf15fbcac818a37ae44cd627be2592cb4f9a3
Michelle Fridlund <michelle.fridlund@gmail.com>
2022-07-29 17:13:58 +0200

    CORRECT norm IQR/Generalise dir search

--- torch/save_train_data.py
+++ torch/save_train_data.py
@@ -7,11 +7,14 @@ import nibabel as nib
 from pathlib import Path
 from scipy.stats import iqr
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import seaborn as sns
 
 req_files = os.listdir('/homes/claes/data_shared/Rb82_train/8cd25543-c065-11ec-b751-e3702ec34f99')
+
+#req_files = ['random_gate_25p.nii.gz', 'random_gate_100p.nii.gz', 'static_100p.nii.gz', 'static_25p.nii.gz']
 
 gated_files_ld = ['gate0_STRESS_25p.nii.gz', 'gate1_STRESS_25p.nii.gz', 'gate2_STRESS_25p.nii.gz', 'gate3_STRESS_25p.nii.gz', 'gate4_STRESS_25p.nii.gz', 'gate5_STRESS_25p.nii.gz', 'gate6_STRESS_25p.nii.gz', 'gate7_STRESS_25p.nii.gz', 'gate0_REST_25p.nii.gz', 'gate1_REST_25p.nii.gz', 'gate2_REST_25p.nii.gz', 'gate3_REST_25p.nii.gz', 'gate4_REST_25p.nii.gz', 'gate5_REST_25p.nii.gz', 'gate6_REST_25p.nii.gz', 'gate7_REST_25p.nii.gz']
 
@@ -33,14 +36,19 @@ def find_nifti(pt):
 def find_patients(args):
     data = args.data
     paths = {}
-    patients = os.listdir(data) 
+    patients = os.listdir(data)
     for p in patients:
-        path = os.path.join(data, p)
-        # Check if patient dir contains all required the files
-        result = all(files in os.listdir(path) for files in req_files)
-        if result:
-            # Return name and full path
-            paths[p] = path
+        if '.pickle' not in p: 
+            path = os.path.join(data, p)
+            if data == '/homes/claes/data_shared/Rb82_train/':
+                # Check if patient dir contains all required the files
+                result = all(files in os.listdir(path) for files in req_files)
+                if result:
+                    # Return name and full path
+                    paths[p] = path
+            else:
+                paths[p] = path
+    # Avoid any previously generated .pickle
     return paths
 
 # Split patients in train/valid
@@ -73,11 +81,19 @@ def nib2np(p):
 
 # Calculate normalisation constant
 def get_iqr(vals):
+    return iqr(vals, rng=(0, 95))
+
+# Calculate normalisation constant
+def get_iqr2(vals):
     return iqr(vals, rng=(0, 98))
 
 # Calculate max intensity
 def get_max(vals):
     return np.max(vals)
+
+# Calculate max intensity
+def get_mean(vals):
+    return np.mean(vals)
 
 # Return a dict of individual IQR and MAX pixel values per patient
 def get_stats(args):
@@ -91,18 +107,22 @@ def get_stats(args):
         values = {
             'IQR': [],
             'MAX': [],
+            'MEAN': [],
         }
         # Returns full path to file
         nifti = find_nifti(path)
         for n in nifti:
     # TODO: add image type as parser instread/
-            if 'static_REST_100p' in n or 'static_STRESS_100p' in n:
-            #if os.path.basename(n) in gated_files_ld: #n is a full path
+            #if 'static_REST_25p' in n or 'static_STRESS_25p' in n:
+            if 'static_25p.nii.gz' in n:
+            #if os.path.basename(n) in gated_files_hd: #n is a full path
                 pixels = nib2np(n)
                 iqr_value = get_iqr(pixels)
                 max_value = get_max(pixels)
+                mean_value = get_mean(pixels)
                 values['IQR'].append(iqr_value)
                 values['MAX'].append(max_value)
+                values['MEAN'].append(mean_value)
 
         stat_dict[name] = values
 
@@ -114,39 +134,52 @@ def plot_values(args):
     # Extract individual values - rewrite later
     iqrs =[]
     maxs = []
+    means = []
 
     for k,v in data.items():
         for val in v['IQR']:
             iqrs.append(val)
         for val in v['MAX']:
             maxs.append(val)
+        for val in v['MEAN']:
+            means.append(val)
 
     
-    dpd = pd.Series(iqrs)
+    dpd = pd.Series(maxs)
     # Median  
-    median_val  = np.median(iqrs)
-    mean_val  = np.mean(iqrs)
+    median_val  = np.median(maxs)
+    mean_val  = np.mean(maxs)
 
     main_iqr = get_iqr(maxs)
+    main_iqr2 = get_iqr2(maxs)
 
-    print("98th percentile of max SUV across training patients: {:.1f}".format(main_iqr))
+    #grand_mean = get_mean(means)
+    #print("Grand mean static HD: {:.1f}".format(grand_mean))
 
-    #out = open('/homes/michellef/clinical_eval/May9/iqr_static.txt', 'w')
-    #out.write(f'Median: {median_val} \n')
-    #out.write(f'Mean: {mean_val} \n')
-    #out.close()
+    print("98th percentile of max SUV across training patients: {:.1f}".format(main_iqr2))
+
 
     # Histogram
     #dpd.plot.hist(grid=False, bins=20, rwidth=0.9, color='#607c8e')
     dpd.plot(style = '.', color='#607c8e')
-    plt.axhline(y = median_val, color = 'r', linestyle = '-')
-    plt.text(-130.0, median_val, "{:.1f}".format(median_val), horizontalalignment='left', size='medium', color='r', alpha = 0.8)
-    plt.title('Gated IQR (0, 99.5) values for each patient (rest + stress)')
-    #plt.title('Static maximal pixel values for each patient (rest + stress)')
+    #plt.axhline(y = mean_val, color = 'r', linestyle = '-')
+    plt.axhline(y = main_iqr2, color = 'r', linestyle = '-')
+    #plt.text(1000.0, 1100000, "Mean:", horizontalalignment='left', size='small', color='r', alpha = 0.8)
+    #plt.text(990.0, mean_val, "{:.1f}".format(mean_val), horizontalalignment='left', size='small', color='r', alpha = 0.8)
+    plt.text(992.0, main_iqr2+50000, "iqr(0,98):", horizontalalignment='left', size='small', color='r', alpha = 0.8)
+    plt.text(986.0, main_iqr2-50000, "{:.1f}".format(main_iqr2), horizontalalignment='left', size='small', color='r', alpha = 0.8)
+    plt.title('25% maximal pixel intensities per patient scan (static rest + stress)')
     plt.xlabel('Patient #')
-    plt.ylabel('IQR')
-    #plt.ylabel('MAX I')
-    plt.savefig('/homes/michellef/clinical_eval/May9/1_iqr_static_ld_99p.png')
+    #plt.ylabel('IQR')
+    plt.ylabel('MAX intensity value (normalised)')
+    plt.savefig('/homes/michellef/clinical_eval/static_ld_maxint_norm.png')
+    plt.close("all")
+
+    dpd.plot.box(color='#607c8e')
+    plt.title('25% maximal pixel intensities per patient scan (static rest + stress)')
+    plt.ylabel('MAX Intensity value (normalised)')
+    plt.xlabel('patient image')
+    plt.savefig('/homes/michellef/clinical_eval/static_ld_maxint_box_norm.png')
     plt.close("all")
 
 
@@ -155,7 +188,7 @@ if __name__ == "__main__":
     parser.add_argument('--data', dest='data', type=str, 
                         default='/homes/claes/data_shared/Rb82_train/',
                         help='data source directory')
-    parser.add_argument("-p", "--plot", help="increase output verbosity", 
+    parser.add_argument("-p", "--plot", help="plot values",
                         action="store_true")
 
     args = parser.parse_args()
